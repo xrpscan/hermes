b35794f70f4e4eaeb7ef477c8545de5dee08ee61
Anurag <anurag@xrpscan.com>
2022-08-17 03:11:51 +0530

    Switched from yarn to npm

--- bin/build-protos.sh
+++ bin/build-protos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-yarn run grpc_tools_node_protoc \
+npx grpc_tools_node_protoc \
   --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
   --ts_out=grpc_js:./src/protos/pb \
   --js_out=import_style=commonjs:./src/protos/pb \
